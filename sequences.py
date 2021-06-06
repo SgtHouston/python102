5cf05e0218cc5c8a9c3a60a4d73a7c3c352bf9a8
Chris Houston <chrisbcoding247@gmail.com>
2021-06-06 14:02:39 -0400

    Updated Sequence.py w/notes

--- sequences.py
+++ sequences.py
@@ -1,5 +1,49 @@
-# Reversing a list
+# Examples of a sequence
+# 
+# List - Mutable (modifiable)
+programming_languages = ["bash", "python", "HTML", "CSS", "Javascript", "SQL" ]
 
+# # Reversing a list
 list1 = ["a", "b", "c", "d", "e"]
 
 print(list1[::-1])
+
+
+
+# Tuple - Immutable sequence
+gps_coordinates = (33.848673, -84.373313)
+
+
+# Range = list of numeric values
+Numbers_from_zero_to_million = range(1000000)
+
+
+# Ways to Modify a list
+# 1. .append() individual items
+# 2. Concatenate two lists together
+# 3. .extend() a list using elements from another list.
+# Concatenation produces a NEW LIST!
+# Append and Extend mutaate a list in-place 
+
+#To DELETE an item from a list, refer to that item and 
+# use the 'del' keyword
+
+list3 = ['a', 'b', 'c']
+del list3[1]
+print(list3)
+
+# to Turn a string into a list 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(alphabet)
+
+
+alpha_list = list(alphabet)
+print(alpha_list)
+
+# Turn a list into a string
+alpha_string = ''.join(alpha_list)
+print(alpha_string)
+
+
+
+
