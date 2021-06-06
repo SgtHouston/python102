50266cc8944b8c1b3dd796296a30f67528337b1d
Chris Houston <chrisbcoding247@gmail.com>
2021-06-06 14:36:55 -0400

    Updated Sequences.py Notes

--- sequences.py
+++ sequences.py
@@ -23,10 +23,10 @@ Numbers_from_zero_to_million = range(1000000)
 # 2. Concatenate two lists together
 # 3. .extend() a list using elements from another list.
 # Concatenation produces a NEW LIST!
-# Append and Extend mutaate a list in-place 
+# Append and Extend mutate a list in-place 
 
-#To DELETE an item from a list, refer to that item and 
-# use the 'del' keyword
+#To DELETE an item from a list, refer to that item using 
+# index and use the 'del' keyword
 
 list3 = ['a', 'b', 'c']
 del list3[1]
@@ -50,6 +50,11 @@ print(alpha_string)
 alpha_string = '-'.join(alpha_list)
 print(alpha_string)
 
+# Turn a list into a string separated by a 
+# character and new line
+
+alpha_string = '!\n'.join(alpha_list)
+print(alpha_string)
 
 
 
